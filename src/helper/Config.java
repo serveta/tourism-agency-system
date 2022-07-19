781d9349b5e341d136e87320df88a3de83b76d1b
Servet Arslan <arslannservett@gmail.com>
2022-07-19 10:14:06 +0300

    Added DBConnector and Config classes

---
+++ src/helper/Config.java
@@ -0,0 +1,8 @@
+package helper;
+
+public class Config {
+    public static final String PROJECT_TITLE = "Tourism Agency System";
+    public static final String DB_URL = "jdbc:postgresql:tourism_agency";
+    public static final String DB_USERNAME = "postgres";
+    public static final String DB_PASSWORD = "postgresql";
+}
